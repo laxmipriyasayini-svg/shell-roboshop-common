e087233477160330abc997684bacbac2d85eb3b5
laxmipriya <laxmipriyasayini@gmail.com>
2026-02-04 15:15:09 +0530

    actions

--- common.sh
+++ common.sh
@@ -7,7 +7,9 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+SCRIPT_DIR=$PWD
 START_TIME=$(date +%s)
+MONGODB_HOST=mongodb.daws88sdev.online
 
 mkdir -p $LOGS_FOLDER
 
@@ -31,11 +33,60 @@ VALIDATE(){
     fi
 }
 
-# print_total_time(){
-# END_TIME=$(date +%s)
-# TOTAL_TIME=$(( $END_TIME - $START_TIME))
-# echo -e "$(date "+%Y-%m-%d %H:%M:%S") | Script execute in: $G $TOTAL_TIME seconds $N" | tee -a $LOGS_FILE
-# }
+nodejs_setup(){
+    dnf module disable nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "Disabling NODEJS Default version"
+
+    dnf module enable nodejs:20 -y &>>$LOGS_FILE
+    VALIDATE $? "Enabling NODEJS 20"
+
+    dnf install nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "Install NODEJS"
+
+     npm install  &>>$LOGS_FILE
+    VALIDATE $? "Installing dependencies"
+}
+
+app_setup(){
+ # creating system user
+    id roboshop &>>LOGS_FILE
+    if [ $? -ne 0 ]; then
+       useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+       VALIDATE $? "Creating system user"
+    else
+      echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+    fi
+
+    mkdir -p /app
+   VALIDATE $? "Creating app directory"
+
+   curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOGS_FILE
+   VALIDATE $? "Downloading $app_name code"
+
+   cd /app
+   VALIDATE $? "Moving to app directory"
+
+   rm -rf /app/*
+   VALIDATE $? "Removing existing code"
+
+   unzip /tmp/$app_name.zip
+   VALIDATE $? "Unzip $app_name code"
+}
+
+systemd_setup(){
+       cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
+       VALIDATE $? "Created systemctl service"
+
+       systemctl daemon-reload
+       systemctl enable $app_name  &>>$LOGS_FILE
+       systemctl start $app_name
+       VALIDATE $? "Starting and enabling $app_name"
+}
+
+app_restart(){
+     systemctl restart $app_name
+     VALIDATE $? "Restarting $app_name"
+}
 
 print_total_time(){
     END_TIME=$(date +%s)
